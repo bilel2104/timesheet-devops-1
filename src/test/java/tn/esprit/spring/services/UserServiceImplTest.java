9320b31af2107ede9865fd486febb649d110ee64
bilel dhouaibi <bilel.dhouaibi@esprit.tn>
2021-11-16 12:54:30 +0100

    4rd update

--- src/test/java/tn/esprit/spring/services/UserServiceImplTest.java
+++ src/test/java/tn/esprit/spring/services/UserServiceImplTest.java
@@ -32,7 +32,7 @@ public class UserServiceImplTest {
 		public void testRetrieveAllUsers() {
 			List<User> listUsers = us.retrieveAllUsers(); 
 			// if there are 7 users in DB : 
-			Assertions.assertEquals(22,listUsers.size());
+			Assertions.assertEquals(23,listUsers.size());
 		
 			
 		}
