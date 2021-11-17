9baa7887ba78362f830b84d071874bb845c8a5f4
bilel dhouaibi <bilel.dhouaibi@esprit.tn>
2021-11-17 13:03:02 +0100

    lastupdate

--- src/main/java/tn/esprit/spring/services/UserServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/UserServiceImpl.java
@@ -2,10 +2,10 @@ package tn.esprit.spring.services;
 
 import java.util.List;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import tn.esprit.spring.entities.User;
 import tn.esprit.spring.repository.UserRepository;
@@ -16,8 +16,7 @@ public class UserServiceImpl implements IUserService {
 	@Autowired
 	UserRepository userRepository;
 
-	private static final Logger l =LogManager.getLogger(UserServiceImpl.class);
-	
+	private static final Logger l = LogManager.getLogger(UserServiceImpl.class);	
 	@Override
 	public List<User> retrieveAllUsers() { 
 		List<User> users = null; 
@@ -26,12 +25,11 @@ public class UserServiceImpl implements IUserService {
 			l.info("In retrieveAllUsers() : ");
 			users = (List<User>) userRepository.findAll();  
 			for (User user : users) {
-				l.debug("user +++ : " + user);
+				l.debug("user +++ : ", user);
 			} 
 			l.info("Out of retrieveAllUsers() : ");
 		}catch (Exception e) {
-			l.error("Error in retrieveAllUsers() : " + e);
-		}
+			l.error("error methode retrieveAll: "+ e);		}
 
 		return users;
 	}
@@ -39,33 +37,54 @@ public class UserServiceImpl implements IUserService {
 
 	@Override
 	public User addUser(User u) {
-		l.info("In method Add User");
-		User uSaved = userRepository.save(u);
-		l.info("Utilisateur Ajoute");
-		return uSaved;
+		User userSaved = new User();
+		try {
+			l.info("In addUser() : ");
+			userSaved = userRepository.save(u);
+			l.debug("user added +++ : ", userSaved);
+			
+			l.info("Out of addUser() : ");
+
+		}catch (Exception e) {
+			l.error("Error in addUser() : ", e);
+		}
+		
+		return userSaved;
 	}
 
 	@Override 
 	public User updateUser(User u) { 
-		l.info("update en cours");
-		User uSaved = userRepository.save(u);
-		l.info("update done!!");
-		return uSaved;
+		l.info("In updateUser() : ");
+		User userSaved = userRepository.save(u);
+		l.debug("user updated  +++ : ",userSaved);
+
+		l.info("Out of updateUser() : ");
+		return userSaved;
 	}
 
 	@Override
 	public void deleteUser(String id) {
-		l.info("Deleting User ...");
+
+		l.info("In deleteUser() : ");
 		userRepository.deleteById(Long.parseLong(id));
-		l.info("User {} deleted succesfully!",id);
+		l.debug("user deleted with id: ", id);
+		l.info("Out of deleteUser() : ");
 	}
 
 	@Override
 	public User retrieveUser(String id) {
-		l.info("Recherche user en cours...");
-		User u =  userRepository.findById(Long.parseLong(id)).orElse(null);
-		l.info("user {} retrieved",u);
-		return u;
+
+		User u = null;
+
+		try {
+			l.info("In retrieveUser() : ");
+
+			u =  userRepository.findById(Long.parseLong(id)).get(); 
+			l.info("Out retrieveUser() : ");
+
+		} catch (Exception e) {
+		}
+		return u; 
 	}
 
 }
